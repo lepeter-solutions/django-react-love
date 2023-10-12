3150227d5740ed00929c7afd931b5f29beeda429
lepeter-solutions <peti.lencses123@gmail.com>
2023-10-12 20:55:33 +0200

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React from 'react';
+
+class App extends React.Component {
+  state = { details: [],}
+
+  componentDidMount(){
+    
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res =>{
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+
+    .catch(err=> {})
+  }
+
+
+  render(){
+    return (
+      <div>
+        <header>Data Generated From Django</header>
+        <hr></hr>
+        {this.state.details.map((output, id) => (
+          <div key = {id}>
+            <h2>{output.szolgaltatas}</h2>
+            <h2>{output.arak}</h2>
+
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+
+
+
+export default App;
