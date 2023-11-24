c19ea5f1b63d5d2599df2a76d8bda83f93a7f05f
lepeter-solutions <peti.lencses123@gmail.com>
2023-11-24 01:35:19 +0100

    new functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,68 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Slider from './Slider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-  state = { details: [],}
+function App() {
+  
+  return (
+    <div className = "container-fluid">
+      <GetHeader />
+      <Slider />
+      <GetPriceTable />
+    </div>
+  );
+};
 
-  componentDidMount(){
-    
-    let data;
-    axios.get('http://localhost:8000')
-    .then(res =>{
-      data = res.data;
-      this.setState({
-        details: data
-      });
-    })
+function GetHeader(){
+  return (
+  <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+        <span className="fs-4">Simple header</span>
+      </a>
 
-    .catch(err=> {})
-  }
-
-
-  render(){
-    return (
-      <div>
-        <header>Data Generated From Django</header>
-        <hr></hr>
-        {this.state.details.map((output, id) => (
-          <div key = {id}>
-            <h2>{output.szolgaltatas}</h2>
-            <h2>{output.arak}</h2>
-            
-          </div>
-        ))}
-      </div>
-    )
-  }
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+      </ul>
+    </header>
+  );
 }
 
+
+
+function GetPriceTable(){
+  const [data, setData] = useState([]);
+
+  // Function to fetch data using Axios
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  // Call fetchData on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h2>Szolgáltatások és árak:</h2>
+      <ul>
+        {data.map((post) => (
+          <li key={post.szolgaltatas}>{post.szolgaltatas} - {post.arak} FT</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 
 
---
+++ frontend/src/Slider.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import pc1 from './images/pc1.jpg';
+import pc2 from './images/pc2.jpg';
+import pc3 from './images/pc3.jpg';
+import MDBContainer from 'mdbreact'
+
+function Slider() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+  
+  return (
+    
+        <div className = "container-fluid">
+            <Carousel activeIndex={index} onSelect={handleSelect} >
+            <Carousel.Item>
+                <Image src = {pc1} fluid  />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <Image src = {pc2} fluid  />
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <Image src = {pc3} fluid  />
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+        </div>
+    
+  );
+}
+
+
+export default Slider;
