ec397a947d2bafec61644634aee8a991d30e1d25
lepeter-solutions <peti.lencses123@gmail.com>
2023-12-07 11:31:53 +0100

    added ImgCards

--- portfolioWebpage/webpage/models.py
+++ portfolioWebpage/webpage/models.py
@@ -22,3 +22,10 @@ class SliderSlide(models.Model):
     img = models.ImageField(upload_to='images/')
     def __str__(self):
         return f"{self.title,self.description, self.img}"
+    
+class ImgCards(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='images/')
+    def __str__(self):
+        return f"{self.title,self.description, self.img}"
